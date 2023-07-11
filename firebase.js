6404c66972bb2bbf94d1f26f2a990908027a5a14
Harsh Sindhwal <101384145+Harshsindhwal@users.noreply.github.com>
2023-07-11 09:03:57 +0530

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB-HPDChexXjy2dZpLZFZba9e4HCpNPb4A",
+    authDomain: "harsh-25969.firebaseapp.com",
+    projectId: "harsh-25969",
+    storageBucket: "harsh-25969.appspot.com",
+    messagingSenderId: "829735157260",
+    appId: "1:829735157260:web:f4e11fc063adc7e7cc3829",
+    measurementId: "G-V1LEPDJHZJ"
+  };
